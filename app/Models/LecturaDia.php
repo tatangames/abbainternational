89227f0ed97d5678d4defdb590fa9cbb8827ddb0
tatangames <54828743+tatangames@users.noreply.github.com>
2024-01-23 20:59:13 -0600

    lectura dia

--- app/Models/LecturaDia.php
+++ app/Models/LecturaDia.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class RefranesTextos extends Model
+class LecturaDia extends Model
 {
     use HasFactory;
-    protected $table = 'refranes_textos';
+    protected $table = 'lectura_dia';
     public $timestamps = false;
 }
--- database/migrations/2024_01_24_025746_create_lectura_dia_table.php
+++ database/migrations/2024_01_24_025746_create_lectura_dia_table.php
@@ -11,13 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('guia_guiada', function (Blueprint $table) {
+        Schema::create('lectura_dia', function (Blueprint $table) {
             $table->id();
-
-            $table->date('fecha');
-            $table->boolean('visible');
-            $table->string('imagen');
-            $table->integer('posicion');
+            $table->timestamps();
         });
     }
 
@@ -26,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('guia_guiada');
+        Schema::dropIfExists('lectura_dia');
     }
 };
