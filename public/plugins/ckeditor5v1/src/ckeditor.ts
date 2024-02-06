f0b9465dd873c032370d6597086f914eee50277b
tatangames <54828743+tatangames@users.noreply.github.com>
2024-02-06 15:43:12 -0600

    regreso

--- public/plugins/ckeditor5v1/src/ckeditor.ts
+++ public/plugins/ckeditor5v1/src/ckeditor.ts
@@ -7,23 +7,22 @@ import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
 
 import { Alignment } from '@ckeditor/ckeditor5-alignment';
 import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
-import { Bold, Italic, Underline } from '@ckeditor/ckeditor5-basic-styles';
-import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
+import { Bold, Code, Italic, Underline } from '@ckeditor/ckeditor5-basic-styles';
 import type { EditorConfig } from '@ckeditor/ckeditor5-core';
 import { Essentials } from '@ckeditor/ckeditor5-essentials';
 import { FontFamily, FontSize } from '@ckeditor/ckeditor5-font';
 import { Heading } from '@ckeditor/ckeditor5-heading';
 import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line';
-import { LegacyList } from '@ckeditor/ckeditor5-list';
+import { HtmlComment } from '@ckeditor/ckeditor5-html-support';
+import { Indent } from '@ckeditor/ckeditor5-indent';
+import { List, ListProperties } from '@ckeditor/ckeditor5-list';
 import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
 import {
 	SpecialCharacters,
 	SpecialCharactersArrows,
-	SpecialCharactersEssentials,
-	SpecialCharactersLatin,
-	SpecialCharactersText
+	SpecialCharactersCurrency,
+	SpecialCharactersLatin
 } from '@ckeditor/ckeditor5-special-characters';
-import { TextTransformation } from '@ckeditor/ckeditor5-typing';
 import { Undo } from '@ckeditor/ckeditor5-undo';
 
 // You can read more about extending the build with additional plugins in the "Installing plugins" guide.
@@ -33,22 +32,23 @@ class Editor extends ClassicEditor {
 	public static override builtinPlugins = [
 		Alignment,
 		Autoformat,
-		BlockQuote,
 		Bold,
+		Code,
 		Essentials,
 		FontFamily,
 		FontSize,
 		Heading,
 		HorizontalLine,
+		HtmlComment,
+		Indent,
 		Italic,
-		LegacyList,
+		List,
+		ListProperties,
 		Paragraph,
 		SpecialCharacters,
 		SpecialCharactersArrows,
-		SpecialCharactersEssentials,
+		SpecialCharactersCurrency,
 		SpecialCharactersLatin,
-		SpecialCharactersText,
-		TextTransformation,
 		Underline,
 		Undo
 	];
@@ -58,20 +58,20 @@ class Editor extends ClassicEditor {
 			items: [
 				'heading',
 				'|',
-				'fontFamily',
-				'fontSize',
 				'bold',
 				'italic',
-				'alignment',
 				'bulletedList',
 				'numberedList',
 				'|',
+				'outdent',
+				'indent',
+				'|',
 				'undo',
 				'redo',
-				'horizontalLine',
+				'alignment',
+				'fontFamily',
 				'specialCharacters',
-				'underline',
-				'blockQuote'
+				'underline'
 			]
 		},
 		language: 'es'
